10c3671bb0ff81a9c96cecb5053a144e877e5ef0
Alina Chumachenko <103441578+AlinaChumachenko@users.noreply.github.com>
2025-04-04 15:27:29 +0300

    first commit

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,23 +1,9 @@
-import { ApplicationConfig, importProvidersFrom } from '@angular/core';
+import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
-// import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { routes } from './app.routes';
-
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes),
-    //  importProvidersFrom([BrowserAnimationsModule])
-    ]
+  providers: [provideRouter(routes), provideAnimationsAsync()]
 };
-
-
-
-// import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-// import { provideRouter } from '@angular/router';
-
-// import { routes } from './app.routes';
-
-// export const appConfig: ApplicationConfig = {
-//   providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
-// };
--- src/app/pipes/truncate.pipe.ts
+++ src/app/pipes/truncate.pipe.ts
@@ -1,12 +1,14 @@
-// import { Pipe, PipeTransform } from '@angular/core';
+import { Pipe, PipeTransform } from '@angular/core';
 
-// @Pipe({
-//   name: 'truncate'
-// })
-// export class TruncatePipe implements PipeTransform {
-
-//   transform(value: string, limit: number): string {
-//     return value.length > limit ? value.substring(0, limit) + '...' : value;
-//   }
-
-// }
+@Pipe({
+  name: 'truncate',
+  standalone: true,
+})
+export class TruncatePipe implements PipeTransform {
+  transform(value: string, limit: number = 20, trail: string = '...'): string {
+    if (!value) {
+      return '';
+    }
+    return value.length > limit ? value.substring(0, limit) + trail : value;
+  }
+}
